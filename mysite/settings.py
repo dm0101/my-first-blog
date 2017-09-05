69f8dbdb9905eecfb32ddda4219e83b5b7cf88d6
dm0101 <dhruvmalik0101@gmail.com>
2017-09-05 15:23:12 +0530

    settings changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=3rkksqsg=@!p%7g)vawv(l=6ur7=_g%wp115xm70c-fxl-&@9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'dm0101.pythonanywhere.com']
 
 
 # Application definition
